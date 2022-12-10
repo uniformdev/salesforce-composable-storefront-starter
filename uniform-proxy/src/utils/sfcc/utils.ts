d5e5158b51b4b2f1d3326dc2624e768b490bd609
Alex Shyba <alexs@uniform.dev>
2022-12-10 15:54:46 -0800

    fix: commerce-sdk package

--- uniform-proxy/src/utils/sfcc/utils.ts
+++
@@ -1,7 +0,0 @@
-export const createProductEditUrlConstructor =
-  (adminPanelUrl: string) =>
-  (productId: string): string => {
-    const normalizedBaseUrl = new URL(adminPanelUrl).origin;
-
-    return `${normalizedBaseUrl}/products/edit-product/${productId}`;
-  };
